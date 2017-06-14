918c8b370582962133a55240314739675d64f32d
newOldBay817 <lechnernn@gmail.com>
2017-06-14 09:15:32 -0400

    Update connect_to_seg.R
    rmoved password

--- R/connect_to_seg.R
+++ R/connect_to_seg.R
@@ -15,6 +15,6 @@
 
 connect_to_seg <- function() {
   require(RPostgreSQL)
-  con_seg <-  dbConnect(PostgreSQL(), dbname='analytics', host = "veritas.c3byxptpwzdt.us-east-1.redshift.amazonaws.com", port=5439, user = "mypizza", password = "ji9P329nso9u3joinfcjd034")
+  con_seg <-  dbConnect(PostgreSQL(), )
   con_seg
 }
